76e18c72e1fe1aa868e961ed612e15e4290a9d64
hberger1119 <115906029+hberger1119@users.noreply.github.com>
2024-02-26 16:12:59 -0700

    Security Update for debug

--- plant-pals/Forum/settings.py
+++ plant-pals/Forum/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
