b9ab523b86a43d700c0062d2efb702dd0797394f
ak2466 <ak2466@nau.edu>
2024-02-29 19:13:34 -0700

    modified allowed_hosts

--- Code/plant-pals/Forum/settings.py
+++ Code/plant-pals/Forum/settings.py
@@ -9,14 +9,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-#these variables are defined in the docker-compose.yml file - alex
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "secret_key"
+SECRET_KEY = 'secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "64.23.151.143"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -70,7 +69,7 @@ WSGI_APPLICATION = 'Forum.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / '../db/db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -113,8 +112,9 @@ USE_TZ = True
 
 # for static files; public env
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, "static"),
 ]
 
 # Managing Media
