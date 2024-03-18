f4ecdaf0083874e32286c809c8b215a6ee1e6b3b
ak2466 <ak2466@nau.edu>
2024-03-17 20:22:43 -0700

    Changed database path to match deployment server's

--- Forum/settings.py
+++ Forum/settings.py
@@ -69,7 +69,7 @@ WSGI_APPLICATION = 'Forum.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / '../db/db.sqlite3',
     }
 }
 
