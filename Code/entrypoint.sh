8f0262c51547f2c7efdda8748a394de66b5d6521
ak2466 <ak2466@nau.edu>
2024-03-23 11:52:56 -0700

    added implementation.txt file

--- Code/entrypoint.sh
+++ Code/entrypoint.sh
@@ -4,9 +4,10 @@ echo "Changing directory"
 cd plant-pals
 
 echo "Apply database migrations"
-python manage.py migrate
+python manage.py migrate --run-syncdb
 
 echo "Starting server"
 python manage.py runserver 0.0.0.0:80
 
 exec "$@"
+
